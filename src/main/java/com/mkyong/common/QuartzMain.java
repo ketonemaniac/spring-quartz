116bb60323a5d671de9ef21b9bb2f378f6646658
ketonemaniac <ketonemaniac@gmail.com>
2015-07-07 22:25:31 +0800

    just to test conflict

--- src/main/java/com/mkyong/common/QuartzMain.java
+++ src/main/java/com/mkyong/common/QuartzMain.java
@@ -3,6 +3,8 @@ package com.mkyong.common;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class QuartzMain {
+
+	// the main method
 	public static void main(String[] args) throws Exception {
 		new ClassPathXmlApplicationContext("Spring-Quartz.xml");
 
